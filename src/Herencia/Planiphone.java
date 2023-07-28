50aeb7dd3b5387785ec3b2ae7dfb06c51bb8c4b3
HP <HP@PaviTallulah>
2023-07-28 14:42:40 -0600

    iphone terminado

--- src/Herencia/Planiphone.java
+++ src/Herencia/Planiphone.java
@@ -1,5 +1,28 @@
 package Herencia;
 
-public class Planiphone {
+public class Planiphone extends Plan{
+
+    private String email;
+
+    public Planiphone(int telefono, String nombre, String correo) {
+        super(telefono, nombre);
+        this.email =correo;
+    }
+
+    
+    public double PagoMensual(int mins, int msgs){
+        double total = 22+(0.4 * mins) +(0.1*msgs);
+        return total;
+    }
+    
+    public void print(){
+        super.Print();
+        System.out.println("Email: "+email);
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
     
 }
